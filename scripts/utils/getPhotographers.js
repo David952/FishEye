3011f02eb704234348b2600171c950c427190c7b
David952 <david.amorim@live.fr>
2025-02-06 23:24:44 +0100

    Ajout de 'getPhotographers.js'

---
+++ scripts/utils/getPhotographers.js
@@ -0,0 +1,7 @@
+export async function getPhotographers() {
+
+    const response = await fetch('/data/photographers.json');
+    const photographers = await response.json()
+
+    return photographers;
+}
