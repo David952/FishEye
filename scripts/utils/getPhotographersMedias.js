93df0038c14d7e66b9daafb8b4ff7c4beb3d6686
David952 <david.amorim@live.fr>
2025-02-25 22:02:47 +0100

    Correction du chemin pour GitHub Pages

--- scripts/utils/getPhotographersMedias.js
+++ scripts/utils/getPhotographersMedias.js
@@ -1,13 +1,13 @@
 export async function getPhotographers() {
 
-    const response = await fetch('/data/photographers.json');
+    const response = await fetch('data/photographers.json');
     const photographers = await response.json()
 
     return photographers;
 }
 
 export async function getMedias() {
-    const response = await fetch('/data/photographers.json');
+    const response = await fetch('data/photographers.json');
     const media = await response.json()
 
     return media;
